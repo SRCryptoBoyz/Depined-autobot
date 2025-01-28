e40da34995ec63c3fef222f7019c8436ac72f7fc
CryptoBoyz <160847170+SRCryptoBoyz@users.noreply.github.com>
2025-01-28 05:46:20 -0400

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ import readline from 'readline';
 const BASE_URL = 'https://api.depined.org/api';
 
 const displayBanner = () => {
-  console.log(chalk.green(figlet.textSync('AirdropInsiders', { horizontalLayout: 'default' })));
+  console.log(chalk.green(figlet.textSync('C-BOYZ', { horizontalLayout: 'default' })));
 };
 
 // Format timestamps
